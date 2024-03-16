f2682053cdbebc6baa0b1e12a8c2367c153118bf
paulg44 <paulgarton1984@hotmail.com>
2024-03-16 10:32:24 +0000

    add my work folder file structure and basic componnent

--- client/src/Pages/Homepage/Homepage.js
+++ client/src/Pages/Homepage/Homepage.js
@@ -1,9 +1,16 @@
 // Component for Homepage
+import { Container } from "react-bootstrap";
 import "./Homepage.css";
 import AboutHeader from "./HompageComponents/About/About";
+import MyWork from "./HompageComponents/MyWork/MyWork";
 
 function Homepage() {
-  return <AboutHeader />;
+  return (
+    <Container>
+      <AboutHeader />
+      <MyWork />
+    </Container>
+  );
 }
 
 export default Homepage;
---
+++ client/src/Pages/Homepage/HompageComponents/MyWork/MyWork.js
@@ -0,0 +1,12 @@
+// MyWork Component
+import "./MyWork.css";
+
+function MyWork() {
+  return (
+    <div>
+      <h2>My Work</h2>
+    </div>
+  );
+}
+
+export default MyWork;
