3f135bfa33ffe43fbd20618fac5cc971d0c09cb0
paulg44 <paulgarton1984@hotmail.com>
2024-03-18 17:08:58 +0000

    add some functionality to mobile and projects page

--- client/src/Pages/Homepage/HompageComponents/MyWork/Mobile/Mobile.js
+++ client/src/Pages/Homepage/HompageComponents/MyWork/Mobile/Mobile.js
@@ -1,12 +1,27 @@
 // Mobile Component
 import "./Mobile.css";
 import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-function Mobile() {
+function Mobile({ handleProjectChoice, projectObj }) {
   return (
     <Container className="mobile">
       <div className="outerMobile">
-        <div className="innerMobile"></div>
+        <div className="innerMobile">
+          <div className="links">
+            <p>Links</p>
+            <ul>
+              {projectObj.map((project, index) => (
+                <li
+                  key={index}
+                  onClick={() => handleProjectChoice(project.projectName)}
+                >
+                  <Link>{projectObj.title}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </Container>
   );
--- client/src/Pages/Homepage/HompageComponents/MyWork/MyWork.js
+++ client/src/Pages/Homepage/HompageComponents/MyWork/MyWork.js
@@ -2,12 +2,55 @@
 import "./MyWork.css";
 import { Container } from "react-bootstrap";
 import Mobile from "./Mobile/Mobile";
+import ssjIMG from "../../../../IMG/ssj_homepage.jpg";
+import amhIMG from "../../../../IMG/hifi_1.jpg";
+import { FaBeer, FaBrush } from "react-icons/fa";
+import { useEffect, useState } from "react";
 
 function MyWork() {
+  const [activeProject, setActiveProject] = useState(null);
+
+  function handleProjectChoice(projectName) {
+    const selectedProject = projectObj.find(
+      (project) => project.projectName === projectName
+    );
+    setActiveProject(selectedProject);
+  }
+
+  useEffect(() => {
+    console.log(activeProject, "is the active project");
+  }, [activeProject]);
+
+  const projectObj = [
+    {
+      id: 1,
+      title: "Shardlow St James",
+      img: ssjIMG,
+      description: "ssj",
+      githubLink: "https://github.com/paulg44/shardlow_st_james",
+      webLink: "https://statuesque-basbousa-0726c3.netlify.app/",
+      icon: <FaBeer />,
+    },
+    {
+      id: 2,
+      title: "AMH Decorating",
+      img: amhIMG,
+      description: "AMH",
+      githubLink: "https://github.com/paulg44/shardlow_st_james",
+      webLink: "https://statuesque-basbousa-0726c3.netlify.app/",
+      icon: <FaBrush />,
+    },
+  ];
+
   return (
     <Container className="myWork">
-      <Container className="projects"></Container>
-      <Mobile />
+      <Container className="projects">
+        <h3>{projectObj.title}</h3>
+      </Container>
+      <Mobile
+        handleProjectChoice={handleProjectChoice}
+        projectObj={projectObj}
+      />
     </Container>
   );
 }
