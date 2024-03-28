be5ecb2feef10ed33400b3dceebffd7453d8968f
paulg44 <paulgarton1984@hotmail.com>
2024-03-28 18:57:27 +0000

    add links to github linkend and email address

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -1,17 +1,25 @@
 // Footer Component
+import { Container } from "react-bootstrap";
 import "./Footer.css";
 import { FaGithub, FaLinkedin } from "react-icons/fa6";
 
 function Footer() {
   return (
-    <ul>
-      <li>
-        <FaGithub />
-      </li>
-      <li>
-        <FaLinkedin />
-      </li>
-    </ul>
+    <Container className="footer">
+      <ul>
+        <a href="https://github.com/paulg44" target="_blank" rel="noreferrer">
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/paul-garton-697a87241/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+      </ul>
+      <p>paulgarton1984@hotmail.com</p>
+    </Container>
   );
 }
 
